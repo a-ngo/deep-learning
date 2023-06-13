191726859d9a2015fbfd035f30a8368b2c409e67
a-ngo <a.ngo@gmx.net>
2023-06-13 21:27:43 +0200

    add torch

---
+++ setup/test_torch_setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import torch
+
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print (x)
+else:
+    print ("MPS device not found.")
+
+# exprected output:
+# tensor([1.], device='mps:0')
